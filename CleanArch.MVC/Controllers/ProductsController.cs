d222caf2f0ce0ec4e53043160439fd5d06d3b0f2
badf <bianca.ferreira@gft.com>
2022-06-02 11:11:02 -0300

    CRUD Products done

--- CleanArch.MVC/Controllers/ProductsController.cs
+++ CleanArch.MVC/Controllers/ProductsController.cs
@@ -1,16 +1,24 @@
+using System.IO;
 using System.Threading.Tasks;
+using CleanArch.Application.DTOs;
 using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArch.MVC.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ICategoryService _categoryService;
         private readonly IProductService _productService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment webHostEnvironment)
         {
-            _productService = productService;   
+            _categoryService = categoryService;
+            _productService = productService;
+            _environment = webHostEnvironment;   
         }
 
         [HttpGet]
@@ -18,6 +26,101 @@ namespace CleanArch.MVC.Controllers
         {
             var products = await _productService.GetProducts();
             return View(products);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDTO)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.Add(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDTO);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var productDTO = await _productService.GetById(id);
+            var categories = await _categoryService.GetCategories();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (productDTO == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDTO.CategoryId);
+
+            return View(productDTO);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(ProductDTO productDTO)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.Update(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDTO);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var productDTO = await _productService.GetById(id);
+
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            if(productDTO == null)
+            {
+                return NotFound();
+            }
+
+            return View(productDTO);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _productService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var productDTO = await _productService.GetById(id);
+            var wwwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + productDTO.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+
+            if(id == null)
+            {
+                return NotFound();
+            }
+            if(productDTO == null)
+            {
+                return NotFound();
+            }
+
+            return View(productDTO);
         }
     }
 }
